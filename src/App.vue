f14344680499620e5cb498d9a6b0de1b6be70578
Erick Chandra <erickchandra718@gmail.com>
2021-07-08 20:19:06 +0700

    Fix Bug

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="main">
+    <router-view></router-view>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+
+* {
+  font-family: "Quicksand", sans-serif;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #e3eaef;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,140 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="phone" :class="`${time}`">
+    <div class="search">
+      <input type="text" name="city" id="city" placeholder="Search..." @keyup.enter="getData" v-model="query">
+    </div>
+
+    <div class="details">
+      <h2><i class="fas fa-map-marker-alt"></i> {{result.name}}, {{result.sys.country}}</h2>
+      <p>{{getTime()}}</p>
+
+      <div class="weather">
+        <h3>{{Math.round(result.main.temp)}}°C - {{result.weather[0].main}}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      key: '02ff15f05075a8938af166f92f7ab9a3',
+      url: 'http://api.openweathermap.org/data/2.5/',
+      query: 'Pontianak',
+      time: '',
+      result: {}
+    }  
+  },
+  methods: {
+    getData() {
+      axios(`${this.url}weather?q=${this.query}&units=metric&APPID=${this.key}`).then((res) => {
+        this.result = res.data
+      });
+    },
+    getTime() {
+      let date = new Date();  
+      const arrDate = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      const arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+      let day = arrDate[date.getDay()];
+      let month = arrMonth[date.getMonth()];
+      let year = date.getFullYear();
+
+      return `${day}, ${month} ${year}`;
+    },
+    bgChanger() {
+      let app = document.querySelector("#phone");
+
+      let date = new Date();
+      let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      let arrTime = time.split(" ");
+      
+      this.time = arrTime[arrTime.length -1];
+    }
+  },
+  created() {
+    this.getData();
+    this.getTime();
+    this.bgChanger();
+  },
+  mounted() {
   }
 }
 </script>
+
+<style>
+
+#main {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#phone {
+  border-radius: 10px;
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: 0.3s;
+}
+
+#phone.AM {
+  background-image: url(../assets/morning.png);
+}
+
+#phone.PM {
+  background-image: url(../assets/night.png);
+}
+
+@media screen and (min-width: 400px) {
+  #phone {
+    height: 70vh;
+    width: 300px;
+  }
+}
+
+.search {
+  display: flex;
+  justify-content: center;
+}
+
+.details {
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  flex-direction: column;
+  text-align: center;
+}
+
+.details h2 {
+  margin: 20px 0 0 0;
+}
+
+.details p {
+  margin: 10px 0;
+}
+
+.weather h3 {
+  font-style: italic;
+}
+
+#city {
+  border: none;
+  outline: none;
+  background-color: rgba(255, 255, 255, 0.7);
+  height: 30px;
+  width: 80%;
+  padding: 0 20px;
+  border-radius: 100px;
+  margin: 20px auto;
+}
+
+</style>
